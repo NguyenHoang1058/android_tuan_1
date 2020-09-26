55154b360b3d66b78d1287cc3caed14d3f23afcb
shiroe <fenrirnobaku1058@gmail.com>
2020-09-26 13:57:33 +0700

    finish lab1_1d

--- app/src/main/java/com/oneg/lab1/MainActivity.java
+++ app/src/main/java/com/oneg/lab1/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_lab1_1c);
+        setContentView(R.layout.activity_main_lab1_1b);
     }
 }
